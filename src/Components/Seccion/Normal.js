dbed394117b3b159b7e1742bfb44e94101e70cae
devHectorGa <contacto@devHector.Ga>
2020-12-24 17:51:10 -0500

    Implementando Responsive Design

--- src/Components/Seccion/Normal.js
+++ src/Components/Seccion/Normal.js
@@ -8,21 +8,20 @@ export default function Normal({ e, moveSectionDown }) {
     >
       <div className="veladura"></div>
       <div className="container">
-        <div className="row col-xs-12 col-sm-8 col-md-offset-2">
-          {typeof e.e.titulo !== 'undefined' && e.e.titulo !== '' && (
-            <h2>{e.e.titulo}</h2>
-          )}
-          <div className="textos">
-            {Boolean(e.e.textos)
-              ? e.e.textos.map((t) => (
-                  <div className="parrafo hidden" key={t.parrafo}>
-                    {t.parrafo}
-                  </div>
-                ))
-              : null}
-          </div>
+        {typeof e.e.titulo !== 'undefined' && e.e.titulo !== '' && (
+          <h2>{e.e.titulo}</h2>
+        )}
+        <div className="textos">
+          {Boolean(e.e.textos)
+            ? e.e.textos.map((t) => (
+                <p className="parrafo hidden" key={t.parrafo}>
+                  {t.parrafo}
+                </p>
+              ))
+            : null}
         </div>
       </div>
+
       {!e.last && <aside className="icon-scroll" onClick={moveSectionDown} />}
     </div>
   );
